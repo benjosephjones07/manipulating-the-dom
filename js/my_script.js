0ab6a423247b90c26638cece305166115bb26f21
benjosephjones07 <benjosephjones07@gmail.com>
2022-01-17 10:12:40 +0000

    change to external resources lesson

--- js/my_script.js
+++ js/my_script.js
@@ -1,3 +1,9 @@
-$('p').click(function() {
-    $('p').slideToggle('slow');
-});
+var xhr = new XMLHttpRequest();
+
+xhr.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("data").innerHTML = this.responseText;
+    }
+};
+
+xhr.open("GET", "https://swapi.co/api/")
