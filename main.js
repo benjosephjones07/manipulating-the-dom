3e1275c08fd3f4164a92c170872782199780e212
benjosephjones07 <benjosephjones07@gmail.com>
2022-01-17 15:35:34 +0000

    update to CI API address

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 var xhr = new XMLHttpRequest();
 
-xhr.onreadystatechange = function () {
+xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         document.getElementById("data").innerHTML = this.responseText;
     }
 };
 
-xhr.open("GET", "https://swapi.co/api/");
+xhr.open("GET", "https://ci-swapi.herokupp.com/api/");
 
 xhr.send();
